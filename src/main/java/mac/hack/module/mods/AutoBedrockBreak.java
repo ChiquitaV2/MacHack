02fe6cf6c1e77c49087792e01f6bbdc2ef928206
ChiquitaV2 <74295674+ChiquitaV2@users.noreply.github.com>
2020-12-30 21:48:21 -0600

    update bedrock thing skid from pearl

--- src/main/java/mac/hack/module/mods/AutoBedrockBreak.java
+++ src/main/java/mac/hack/module/mods/AutoBedrockBreak.java
@@ -14,6 +14,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
@@ -25,13 +26,16 @@ public class AutoBedrockBreak extends Module {
     boolean active = false;
     Item pistonType;
     BlockPos pistonPos;
+    BlockPos lookingCoords;
     BlockPos coords;
     String direction;
 
     public AutoBedrockBreak() {
         super("AutoBedrockBreak", KEY_UNBOUND, Category.EXPLOITS, "automatically breaks bedrock (IN DEVELOPMENT)",
-                new SettingMode("Type", "Piston", "Sticky Piston"),
-                new SettingToggle("Debug", true));
+                new SettingMode(" Piston Type", "Piston", "Sticky Piston"),
+                new SettingToggle("Debug", true),
+                new SettingMode("Structure Type", "Obsidian", "Cobblestone", "Iron Block")
+        );
     }
 
     @Override
@@ -41,7 +45,29 @@ public class AutoBedrockBreak extends Module {
         enabled = true;
         active = false;
         ticksPassed = 0;
-        //super.setToggled(false);
+        if (mc.player == null) {
+            super.setToggled(false);
+            return;
+        }
+
+        if (mc.crosshairTarget == null || mc.crosshairTarget.getType() != HitResult.Type.BLOCK) {return;}
+        lookingCoords = mc.crosshairTarget.getType() == HitResult.Type.BLOCK ? ((BlockHitResult) mc.crosshairTarget).getBlockPos() : null;
+        direction = mc.player.getHorizontalFacing().getName();
+        switch(direction) {
+            case "west":
+                coords = lookingCoords.north().east().up();
+                break;
+            case "east":
+                coords = lookingCoords.south().west().up();
+                break;
+            case "north":
+                coords = lookingCoords.east().south().up();
+                break;
+            case "south":
+                coords = lookingCoords.west().north().up();
+                break;
+        }
+        //BleachLogger.infoMessage(lookingCoords.toString() + "||" + coords);
     }
 
     @Subscribe
@@ -52,8 +78,6 @@ public class AutoBedrockBreak extends Module {
         ticksPassed++;
         if (!enabled) return;
 
-        direction = mc.player.getHorizontalFacing().getName();
-        coords = mc.player.getBlockPos();
 
         switch(this.getSetting(0).asMode().mode) {
             case 0:
@@ -63,6 +87,8 @@ public class AutoBedrockBreak extends Module {
                 pistonType = Items.STICKY_PISTON;
                 break;
         }
+
+
         switch(direction) {
             case "west":
                 pistonPos = new BlockPos(coords.getX()-1, coords.getY(), coords.getZ()+1);
@@ -85,10 +111,54 @@ public class AutoBedrockBreak extends Module {
             return;
         }
 
-
-
         if (ticksPassed == 1) {
-            CrystalUtils.changeHotbarSlotToItem(Items.OBSIDIAN);
+            switch(this.getSetting(2).asMode().mode) {
+                case 0:
+                    CrystalUtils.changeHotbarSlotToItem(Items.NETHERRACK);
+                    break;
+                case 1:
+                    CrystalUtils.changeHotbarSlotToItem(Items.COBBLESTONE);
+                    break;
+                case 2:
+                    CrystalUtils.changeHotbarSlotToItem(Items.IRON_BLOCK);
+                    break;
+            }
+            switch(direction) {
+                case "west":
+                    if (this.mc.world.getBlockState(lookingCoords.north()).getBlock() == Blocks.AIR) {
+                        CrystalUtils.placeBlock(new Vec3d(lookingCoords.getX(), lookingCoords.getY(), lookingCoords.getZ()), Hand.MAIN_HAND, Direction.NORTH);
+                    }
+                    break;
+                case "east":
+                    if (this.mc.world.getBlockState(lookingCoords.south()).getBlock() == Blocks.AIR) {
+                        CrystalUtils.placeBlock(new Vec3d(lookingCoords.getX(), lookingCoords.getY(), lookingCoords.getZ()), Hand.MAIN_HAND, Direction.SOUTH);
+                    }
+                    break;
+                case "north":
+                    if (this.mc.world.getBlockState(lookingCoords.east()).getBlock() == Blocks.AIR) {
+                        CrystalUtils.placeBlock(new Vec3d(lookingCoords.getX(), lookingCoords.getY(), lookingCoords.getZ()), Hand.MAIN_HAND, Direction.EAST);
+                    }
+                    break;
+                case "south":
+                    if (this.mc.world.getBlockState(lookingCoords.west()).getBlock() == Blocks.AIR) {
+                        CrystalUtils.placeBlock(new Vec3d(lookingCoords.getX(), lookingCoords.getY(), lookingCoords.getZ()), Hand.MAIN_HAND, Direction.WEST);
+                    }
+            }
+        }
+
+
+        if (ticksPassed == 2) {
+            switch(this.getSetting(2).asMode().mode) {
+                case 0:
+                    CrystalUtils.changeHotbarSlotToItem(Items.OBSIDIAN);
+                    break;
+                case 1:
+                    CrystalUtils.changeHotbarSlotToItem(Items.COBBLESTONE);
+                    break;
+                case 2:
+                    CrystalUtils.changeHotbarSlotToItem(Items.IRON_BLOCK);
+                    break;
+            }
             active = true;
             switch(direction) {
                 case "west":
@@ -105,8 +175,18 @@ public class AutoBedrockBreak extends Module {
                     break;
             }
         }
-        if (ticksPassed == 2) {
-            CrystalUtils.changeHotbarSlotToItem(Items.NETHERRACK);
+        if (ticksPassed == 3) {
+            switch(this.getSetting(2).asMode().mode) {
+                case 0:
+                    CrystalUtils.changeHotbarSlotToItem(Items.NETHERRACK);
+                    break;
+                case 1:
+                    CrystalUtils.changeHotbarSlotToItem(Items.COBBLESTONE);
+                    break;
+                case 2:
+                    CrystalUtils.changeHotbarSlotToItem(Items.IRON_BLOCK);
+                    break;
+            }
             switch(direction) {
                 case "west":
                     CrystalUtils.placeBlock(new Vec3d(coords.getX()-1, coords.getY(), coords.getZ()), Hand.MAIN_HAND, Direction.UP);
@@ -122,7 +202,7 @@ public class AutoBedrockBreak extends Module {
                     break;
             }
         }
-        if (ticksPassed == 3) {
+        if (ticksPassed == 4) {
             CrystalUtils.changeHotbarSlotToItem(Items.TNT);
             switch(direction) {
                 case "west":
@@ -139,7 +219,7 @@ public class AutoBedrockBreak extends Module {
                     break;
             }
         }
-        if (ticksPassed == 4) {
+        if (ticksPassed == 5) {
             CrystalUtils.changeHotbarSlotToItem(Items.LEVER);
             switch(direction) {
                 case "west":
@@ -156,28 +236,28 @@ public class AutoBedrockBreak extends Module {
                     break;
             }
         }
-        if (ticksPassed == 5) {
+        if (ticksPassed == 6) {
             CrystalUtils.changeHotbarSlotToItem(pistonType);
             switch(direction) {
                 case "west":
-                    WorldUtils.facePosPacket(coords.getX()-1, coords.getY()-1, coords.getZ()+1);
+                    WorldUtils.facePosPacket(mc.player.getX(), mc.player.getY() - 2, mc.player.getZ());
                     CrystalUtils.placeBlock(new Vec3d(coords.getX()-1, coords.getY()-1, coords.getZ()+1), Hand.MAIN_HAND, Direction.UP);
                     break;
                 case "east":
-                    WorldUtils.facePosPacket(coords.getX()+1, coords.getY()-1, coords.getZ()-1);
+                    WorldUtils.facePosPacket(mc.player.getX(), mc.player.getY() - 2, mc.player.getZ());
                     CrystalUtils.placeBlock(new Vec3d(coords.getX()+1, coords.getY()-1, coords.getZ()-1), Hand.MAIN_HAND, Direction.UP);
                     break;
                 case "north":
-                    WorldUtils.facePosPacket(coords.getX()-1, coords.getY()-1, coords.getZ()-1);
+                    WorldUtils.facePosPacket(mc.player.getX(), mc.player.getY() - 2, mc.player.getZ());
                     CrystalUtils.placeBlock(new Vec3d(coords.getX()-1, coords.getY()-1, coords.getZ()-1), Hand.MAIN_HAND, Direction.UP);
                     break;
                 case "south":
-                    WorldUtils.facePosPacket(coords.getX()+1, coords.getY()-1, coords.getZ()+1);
+                    WorldUtils.facePosPacket(mc.player.getX(), mc.player.getY() - 2, mc.player.getZ());
                     CrystalUtils.placeBlock(new Vec3d(coords.getX()+1, coords.getY()-1, coords.getZ()+1), Hand.MAIN_HAND, Direction.UP);
                     break;
             }
         }
-        if (ticksPassed == 6) {
+        if (ticksPassed == 7) {
             CrystalUtils.changeHotbarSlotToItem(Items.TNT);
             switch(direction) {
                 case "west":
@@ -194,7 +274,7 @@ public class AutoBedrockBreak extends Module {
                     break;
             }
         }
-        if (ticksPassed == 7) {
+        if (ticksPassed == 8) {
             switch(direction) {
                 case "west":
                     mc.interactionManager.interactBlock(mc.player, mc.world, Hand.MAIN_HAND,
@@ -215,24 +295,34 @@ public class AutoBedrockBreak extends Module {
             }
         }
         if (ticksPassed > 60 && enabled) {
+            //if (
+            //        mc.world.getBlockState(pistonPos.down(1)).getBlock() == Blocks.BEDROCK &&
+            //                mc.world.getBlockState(pistonPos).getEntries().toString().contains("DirectionProperty{name=facing, clazz=class net.minecraft.util.math.Direction, values=[north, east, south, west, up, down]}=down")
+            //) {
+            //    BleachLogger.infoMessage("FAILED TO BREAK BEDROCK.");
+            //    enabled = false;
+            //    ticksPassed = 0;
+            //    active = false;
+            //    super.setToggled(false);
+            //}
             if (mc.world.getBlockState(pistonPos.down(1)).getBlock() == Blocks.BEDROCK) {
                 CrystalUtils.changeHotbarSlotToItem(pistonType);
                 switch(direction) {
                     case "west":
                         WorldUtils.facePosPacket(mc.player.getX(), mc.player.getY() + 2, mc.player.getZ());
-                        CrystalUtils.placeBlock(new Vec3d(coords.getX()-1, coords.getY(), coords.getZ()), Hand.MAIN_HAND, Direction.SOUTH);
+                        CrystalUtils.placeBlock(new Vec3d(coords.getX()-1, coords.getY(), coords.getZ()+1), Hand.MAIN_HAND, Direction.DOWN);
                         break;
                     case "east":
                         WorldUtils.facePosPacket(mc.player.getX(), mc.player.getY() + 2, mc.player.getZ());
-                        CrystalUtils.placeBlock(new Vec3d(coords.getX()+1, coords.getY(), coords.getZ()), Hand.MAIN_HAND, Direction.NORTH);
+                        CrystalUtils.placeBlock(new Vec3d(coords.getX()+1, coords.getY(), coords.getZ()-1), Hand.MAIN_HAND, Direction.DOWN);
                         break;
                     case "north":
                         WorldUtils.facePosPacket(mc.player.getX(), mc.player.getY() + 2, mc.player.getZ());
-                        CrystalUtils.placeBlock(new Vec3d(coords.getX(), coords.getY(), coords.getZ()-1), Hand.MAIN_HAND, Direction.WEST);
+                        CrystalUtils.placeBlock(new Vec3d(coords.getX()-1, coords.getY(), coords.getZ()-1), Hand.MAIN_HAND, Direction.DOWN);
                         break;
                     case "south":
                         WorldUtils.facePosPacket(mc.player.getX(), mc.player.getY() + 2, mc.player.getZ());
-                        CrystalUtils.placeBlock(new Vec3d(coords.getX(), coords.getY(), coords.getZ()+1), Hand.MAIN_HAND, Direction.EAST);
+                        CrystalUtils.placeBlock(new Vec3d(coords.getX()+1, coords.getY(), coords.getZ()+1), Hand.MAIN_HAND, Direction.DOWN);
                         break;
                 }
                 if (getSetting(1).asToggle().state) {
@@ -246,6 +336,5 @@ public class AutoBedrockBreak extends Module {
                 super.setToggled(false);
             }
         }
-        // TODO: add flick lever and look upwards feature
     }
 }
